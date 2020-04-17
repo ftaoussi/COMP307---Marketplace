bf62635c436356058eca94a8e8934b5d3f4f85ec
Faisal Rabbani <faisal.rabbani@mail.mcgill.ca>
2020-04-17 15:47:43 -0400

    test

--- product_listing/forms.py
+++ product_listing/forms.py
@@ -2,16 +2,16 @@ from django import forms
 from smart_selects.db_fields import ChainedForeignKey
 
 class ListingForm(forms.Form):
-    name = models.CharField()
+    name = forms.CharField()
     seller = request.user
-    price_initial = models.IntegerField()
-    category = model.ChoiceField(queryset = Category.objects.all())
+    price_initial = forms.IntegerField()
+    category = forms.ChoiceField(queryset = Category.objects.all())
     #the line below is for the chained/dependent drop down list
-    subcategory = model.ChainedForeignKey(Subcategory, chained_field="category", chained_model_field="category", show_all = False, auto_choose = True, sort=True)
-    location = model.ChoiceField(queryset = Neighborhood.objects.all())
-    time = models.TimeField()
-    description = models.CharField()
-    stock = models.IntegerField()
-    size = model.CharField()
-    image = model.ImageField()
+    subcategory = forms.ChainedForeignKey(Subcategory, chained_field="category", chained_model_field="category", show_all = False, auto_choose = True, sort=True)
+    location = forms.ChoiceField(queryset = Neighborhood.objects.all())
+    time = forms.TimeField()
+    description = forms.CharField()
+    stock = forms.IntegerField()
+    size = forms.CharField()
+    image = forms.ImageField()
     
