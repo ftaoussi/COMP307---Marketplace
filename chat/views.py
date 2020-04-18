34706482498f7e77e5614bdc55ea690a6d1f76b2
ftaoussi <63752233+ftaoussi@users.noreply.github.com>
2020-04-17 20:41:47 -0400

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    return render(request, 'chat/index.html')
+    return render(request, 'chat/index.html', {})
 
-def room(request, room_name):
+def room(request, room_name=None):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+    
