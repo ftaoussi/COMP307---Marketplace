b799bf3a3acb90b529e2a364abcdd9d1630ca510
ftaoussi <63752233+ftaoussi@users.noreply.github.com>
2020-04-20 11:33:40 -0400

    added redirect import

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.db import IntegrityError
 from django.contrib.auth.models import User
