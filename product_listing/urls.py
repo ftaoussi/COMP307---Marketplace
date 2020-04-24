fc5a595df48e661ec5f5b2088a7ce07b9602f5b0
ftaoussi <63752233+ftaoussi@users.noreply.github.com>
2020-04-23 20:03:20 -0400

    Update urls.py

--- product_listing/urls.py
+++ product_listing/urls.py
@@ -6,7 +6,7 @@ app_name = 'product_listing'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('modify_listing/', views.modify_listing, name ='modify_listing'),
-	path('delete_listing/', views.delete_listing, name ='delete_listing'),
+	path('<int:id>/delete_listing/', views.delete_listing, name ='delete_listing'),
 	path('list/', views.listItem, name ='listItem'),
 	path('<int:product_id>', views.viewItem)
 ]
