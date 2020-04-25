b2c78c479514aa16e8352671f6ecd04ce84b5d1b
snordale <snordale@gmail.com>
2020-04-24 20:49:39 -0700

    style stuff

--- product_listing/static/product_listing/script.js
+++ product_listing/static/product_listing/script.js
@@ -12,16 +12,13 @@ $('.single-filter-option').click(function() {
 function displayListings() {
 	var selectedClasses = []
 
-	console.log('here are the selected filters')
 	var $selectedFilters = $('.selected').each(function () {
-		console.log($(this).html())
 		selectedClasses.push($(this).html())
 	}); 
 
 	console.log("selectedClasses: " + selectedClasses)
 
 	if ($selectedFilters.length == 0) {
-		console.log('$selectedFilters is empty')
 		$('.single-filter-option').css('color', 'black')
 		$('.listing').fadeIn( "fast" )
 	}
@@ -29,18 +26,15 @@ function displayListings() {
 		console.log('$selectedFilters is not empty')
 		$('.single-filter-option').css('color', '#777777')
 		$('.selected').css('color', 'black')
-		console.log(typeof selectedClasses)
 		// console.log($selectedFilters)
 		$('.listing').each(function( i ) {
 			console.log($(this))
 			if (shouldListingDisplay(selectedClasses, this.classList)) {
-				console.log(shouldListingDisplay(selectedClasses, this.classList))
 				$(this).fadeIn('fast')
 				this.style.display = 'inline'
 			} else {
 				console.log(shouldListingDisplay(selectedClasses, this.classList))
 				$(this).fadeOut('fast')
-				
 			}
 		});
 	}
